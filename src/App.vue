80ecad81b9257ac70d14cbf0ca0c63c0041c001f
System Administrator <root@Sergeis-MacBook-Pro.local>
2024-04-22 20:42:07 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <script setup>
 import { ref, computed, onMounted, onUnmounted } from 'vue';
 
-const dataSlides = ref(null);
-
-const fetchData = async () => {
-  try {
-    const response = await fetch('https://picsum.photos/v2/list?page=2&limit=5');
-    const jsonData = await response.json();
-    dataSlides.value = jsonData;
-
-  } catch (error) {
-    console.error('Ошибка при получении данных:', error);
-  }
-};
-
-const currentIndex = ref(0);
-
+const dataSlides = ref([]);
 let slideWidth = ref(450);
 let sliderWidth = ref(null)
 let test = ref(0)
 
 let count = ref(0);
 
+const transformData = (data) => {
+  return data.map(item => ({
+    ...item,
+    isChecked: false
+  }))
+}
+
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://picsum.photos/v2/list?page=2&limit=5');
+    const jsonData = await response.json();
+    dataSlides.value = transformData(jsonData);
+
+  } catch (error) {
+    console.error('error', error);
+  }
+};
 
 const prevSlide = () => {
   
@@ -41,12 +44,16 @@ const nextSlide = () => {
     dataSlides.value.push(firstSlide)
   } else {
     count.value++;
-    console.log(count.value, 'count.value++')
     test.value = count.value * slideWidth.value;
   }
 };
 
-
+const getImageUrlList = computed(() => dataSlides.value
+  .filter(item => item.isChecked)
+  .map(item => (
+    item.download_url
+  ))
+)
 
 const totalSlides = computed(() => dataSlides.value.length);
 console.log(totalSlides, 'totalSlides');
@@ -55,19 +62,25 @@ onMounted(() => {
   fetchData();
 });
 
-
-
 </script>
 
 <template>
   <div class="slider">
     <div class="wrapper-slider" :style="`transform: translate(-${test}px);`">
-      <template
+      <div
         v-for="(slide, index) in dataSlides"
         :key="slide.id"
       >
-        <img :src="slide.download_url" alt="" :class="{ 'active': count === index}">
-      </template>
+        <input type="checkbox" :id="slide.id" v-model="slide.isChecked" />
+        <label :for="slide.id">
+          <img :src="slide.download_url" alt="" :class="{ 'active': count === index}">
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="table-url">
+    <div class="table-url__cell" v-for="(imageUrl, index) in getImageUrlList" :key="index">
+      {{ imageUrl }}
     </div>
   </div>
   <div class="btn-holder">
